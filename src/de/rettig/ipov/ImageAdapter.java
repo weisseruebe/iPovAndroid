2b724a5bab4b0dec872c66747ab29d228edfc535
weisseruebe <a.rettig@gmx.de>
2011-11-26 02:22:54 +0100

    Refactoring

---
+++ src/de/rettig/ipov/ImageAdapter.java
@@ -0,0 +1,58 @@
+package de.rettig.ipov;
+
+import com.example.android.BluetoothChat.R;
+import com.example.android.BluetoothChat.R.drawable;
+import com.example.android.BluetoothChat.R.styleable;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    int mGalleryItemBackground;
+    private Context mContext;
+
+    private Integer[] mImageIds = {
+            R.drawable.sample_1,
+            R.drawable.sample_2,
+            R.drawable.sample_3,
+            R.drawable.sample_4,
+            R.drawable.sample_5
+             
+    };
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+        TypedArray attr = mContext.obtainStyledAttributes(R.styleable.HelloGallery);
+        mGalleryItemBackground = attr.getResourceId(
+                R.styleable.HelloGallery_android_galleryItemBackground, 0);
+        attr.recycle();
+    }
+
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+
+        imageView.setImageResource(mImageIds[position]);
+        imageView.setLayoutParams(new Gallery.LayoutParams(150, 100));
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageView.setBackgroundResource(mGalleryItemBackground);
+
+        return imageView;
+    }
+}
